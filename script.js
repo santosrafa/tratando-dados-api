f27d9a078d60faf7c9ac28d78515f1dbf249c7c0
Your Name <ti.rafaelteles@gmail.com>
2023-06-05 13:30:29 -0300

    try

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 //Arquivo JavaScript
+async function buscaEndereco(cep) {
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro){
+            throw Error('CEP não existente!');
+        }
+            console.log(consultaCEPConvertida);
+            return consultaCEPConvertida;
+        }catch (erro){
+            console.log(erro)
+        }
+}
 
-var ConsultaCEP = fetch('https://viacep.com.br/ws/01001000/json/');
-
-console.log(ConsultaCEP);
+buscaEndereco();
